c94d5ae167939af2ea306642683068490529ff19
Srini <srini1201@gmail.com>
2018-05-30 16:39:13 +0530

    Dev comment added

--- app/src/main/java/com/example/srinivasan/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/srinivasan/myapplication/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //Update in new branch
         //Local new branch
+        //Dev local test
     }
 }
